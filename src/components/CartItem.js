748d01a2bcf8ae6ecd90b4aa70410a900e341782
Omar Suriel <omarsuriel@icloud.com>
2022-09-27 13:33:35 -0400

    Added the shopping cart

---
+++ src/components/CartItem.js
@@ -0,0 +1,49 @@
+import {
+  Card, CardMedia, Box, Typography, IconButton,
+} from '@mui/material';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+
+function CartItem(props) {
+  const { cartItem, removeFromCart } = props;
+
+  return (
+    <Card>
+      <Box display="flex" width="100%">
+        <Box>
+          <CardMedia
+            component="img"
+            sx={{ width: 80, height: 80, p: 1 }}
+            image={cartItem.image}
+          />
+        </Box>
+        <Box px={2} display="flex" flexDirection="column" flexGrow={1} justifyContent="center">
+          <Box>
+            <Typography fontWeight="bold">
+              {cartItem.title}
+            </Typography>
+          </Box>
+          <Box>
+            <Typography fontWeight="bold">
+
+              $
+              {cartItem.price / 100}
+            </Typography>
+          </Box>
+        </Box>
+        <Box px={2} display="flex" flexDirection="column" justifyContent="center">
+          <Typography fontWeight="bold">
+            x
+            {cartItem.quantity}
+          </Typography>
+        </Box>
+        <Box px={2} display="flex" flexDirection="column" justifyContent="center" alignItems="center">
+          <IconButton onClick={() => removeFromCart(cartItem.id)}>
+            <DeleteForeverIcon />
+          </IconButton>
+        </Box>
+      </Box>
+    </Card>
+  );
+}
+
+export default CartItem;
--- src/components/pages/CartPage.js
+++ src/components/pages/CartPage.js
@@ -1,9 +1,13 @@
-import { Box } from '@mui/material';
+import { Box, Button } from '@mui/material';
+import HomeIcon from '@mui/icons-material/Home';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { Link } from 'react-router-dom';
+import CartItem from '../CartItem';
 import Layout from '../layout/Layout';
 
 function CartPage(props) {
   const {
-    shoppingCart, addToCart, removeFromCart, emptyCart,
+    shoppingCart, removeFromCart, emptyCart,
   } = props;
 
   return (
@@ -13,15 +17,39 @@ function CartPage(props) {
         display="flex"
         flexDirection="column"
         alignItems="center"
+        mb={6}
       />
       {shoppingCart.map((cartItem) => (
-        <Box>
-          <div>{cartItem.title}</div>
-          <div>{cartItem.description}</div>
-          <div>{cartItem.price}</div>
-          <div>{cartItem.quantity}</div>
+        <Box mb={3}>
+          <CartItem cartItem={cartItem} removeFromCart={removeFromCart} />
         </Box>
       ))}
+      <Box mt={5}>
+        <Box mb={3}>
+          <Button fullWidth variant="contained">Checkout</Button>
+        </Box>
+        <Box mb={3}>
+          <Button
+            fullWidth
+            variant="contained"
+            startIcon={<DeleteIcon />}
+            onClick={emptyCart}
+          >
+            Empty Cart
+          </Button>
+        </Box>
+        <Box mb={3}>
+          <Link to="/">
+            <Button
+              fullWidth
+              variant="contained"
+              startIcon={<HomeIcon />}
+            >
+              Home
+            </Button>
+          </Link>
+        </Box>
+      </Box>
     </Layout>
   );
 }
