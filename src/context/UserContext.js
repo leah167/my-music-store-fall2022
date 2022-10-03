b3ac4b7fc007054c939ba60a8e198febfc6329e4
Leah Sanchez <leahsanchez@Leahs-MacBook-Pro.local>
2022-10-03 00:50:32 -0600

    replaced useState with useReducer for cart

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,16 +1,39 @@
-import { createContext, useState } from 'react';
+// import { createContext, useState } from 'react';
+import { createContext, useReducer } from "react";
+
+const SIGN_IN = "sign-in";
+const SIGN_OUT = "sign-out";
 
 export const userContext = createContext();
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case SIGN_IN: {
+      return { ...state, user: { ...action.payload.userData } };
+    }
+    case SIGN_OUT: {
+      return undefined;
+    }
+
+    default: {
+      return state;
+    }
+  }
+};
 
 function UserContextProvider(props) {
   const { children } = props;
   const userInitialState = undefined;
 
-  const [user, setUser] = useState(userInitialState);
+  // const [user, setUser] = useState(userInitialState);
+  const [user, dispatch] = useReducer(reducer, userInitialState);
 
-  const signIn = (userData) => setUser(userData);
+  // const signIn = (userData) => setUser(userData);
+  const signIn = (userData) =>
+    dispatch({ type: SIGN_IN, payload: { userData } });
 
-  const signOut = () => setUser(userInitialState);
+  // const signOut = () => setUser(userInitialState);
+  const signOut = () => dispatch({ type: SIGN_OUT });
 
   return (
     <userContext.Provider value={{ user, signIn, signOut }}>
